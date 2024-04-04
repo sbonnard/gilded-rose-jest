83a0970c34ea6daf6493127704f00d8eb3df5370
Guillaume <guillaumebelleuvre@gmail.com>
2024-04-04 14:34:45 +0200

    makes first test failed

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -5,7 +5,7 @@ describe("Gilded Rose item", function () {
   it("should keep name", function () {
     const gildedRose = new Shop([new Item("Stuff", 5, 5)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("Stuff");
+    expect(items[0].name).toBe("product name");
   });
 
 });
