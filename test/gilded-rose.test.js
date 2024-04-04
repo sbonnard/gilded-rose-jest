a41814391882ecdfcd870c830239c9a7548d89b7
Guillaume <guillaumebelleuvre@gmail.com>
2024-04-04 15:01:42 +0200

    remove export module

--- test/gilded-rose.test.js
+++ test/gilded-rose.test.js
@@ -1,6 +1,6 @@
 const { Shop, Item } = require("../src/gilded-rose");
 
-const test = describe("Gilded Rose item", function () {
+describe("Gilded Rose item", function () {
 
   it("should keep name", function () {
     const gildedRose = new Shop([new Item("Stuff", 5, 5)]);
@@ -9,5 +9,3 @@ const test = describe("Gilded Rose item", function () {
   });
 
 });
-
-export default test;
